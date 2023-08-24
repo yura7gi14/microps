a7592c7d0367d07036bc71d88cf7c1101f102fac
yura7gi14 <yui.shiopi@gmail.com>
2023-08-24 23:42:23 +0900

    add:STEP 5まで

--- Makefile
+++ Makefile
@@ -1,13 +1,18 @@
 APPS = 
 
 DRIVERS = driver/dummy.o \
+          driver/loopback.o \
 
 OBJS = util.o \
        net.o \
+       ip.o \
 
 TESTS = test/step0.exe \
         test/step1.exe \
         test/step2.exe \
+        test/step3.exe \
+        test/step4.exe \
+        test/step5.exe \
 
 CFLAGS := $(CFLAGS) -g -W -Wall -Wno-unused-parameter -iquote .
 
--- net.c
+++ net.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
 #include "util.h"
 #include "net.h"
 
+struct net_protocol {
+    struct net_protocol *next;
+    uint16_t type;
+    struct queue_head queue; /* input queue */
+    void (*handler)(const uint8_t *data, size_t len, struct net_device *dev);
+};
+
+struct net_protocol_queue_entry {
+    struct net_device *dev;
+    size_t len;
+    uint8_t data[];
+};
+
 /* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
 static struct net_device *devices;
+static struct net_protocol *protocols;
 
 struct net_device *
 net_device_alloc(void)
@@ -93,13 +108,85 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
     return 0;
 }
 
+/* NOTE: must not be call after net_run() */
+int
+net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t len, struct net_device *dev))
+{
+    struct net_protocol *proto;
+
+    for(proto = protocols; proto; proto = proto->next){
+        if(type == proto->type){
+            errorf("already registered, type=0x%04x", type);
+            return -1;
+        }
+    }
+    proto = memory_alloc(sizeof(*proto));
+    if(!proto){
+        errorf("memory_alloc() failure");
+        return -1;
+    }
+    proto->type = type;
+    proto->handler = handler;
+    proto->next = protocols;
+    protocols = proto;
+    infof("registered, type=0x%04x", type);
+    return 0;
+}
+
 int
 net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
-    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
-    debugdump(data, len);
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for(proto = protocols; proto; proto = proto->next){
+        if(proto->type == type){
+            /*プロトコルの受信キューにエントリを挿入*/
+            entry = memory_alloc(sizeof(*entry) + len);
+            if(!entry){
+                errorf("memory_alloc() failure");
+                return -1;
+            }
+            entry->dev = dev;
+            entry->len = len;
+            memcpy(entry->data, data, len);
+            if(!queue_push(&proto->queue, entry)){
+                errorf("queue_push() failure");
+                memory_free(entry);
+                return -1;
+            }
+
+            debugf("queue pushed (num:%u), dev=%s, type=0x%04x, len=%zu", proto->queue.num, dev->name, type, len);
+            debugdump(data, len);
+            intr_raise_irq(INTR_IRQ_SOFTIRQ);
+            return 0;
+        }
+    }   
+    /*unsupported protocol*/
     return 0;
 }
+
+int
+net_softirq_handler(void)
+{
+    struct net_protocol *proto;
+    struct net_protocol_queue_entry *entry;
+
+    for(proto = protocols; proto; proto = proto->next){
+        while(1){
+            entry = queue_pop(&proto->queue);
+            if(!entry){
+                break;
+            }
+            debugf("queue popped (num:%u), dev=%s, type=0x%04x, len=%zu", proto->queue.num, entry->dev->name, proto->type, entry->len);
+            debugdump(entry->data, entry->len);
+            proto->handler(entry->data, entry->len, entry->dev);
+            memory_free(entry);
+        }
+    }
+    return 0;
+}
+
 
 int
 net_run(void)
@@ -136,6 +223,10 @@ net_init(void)
 {
     if (intr_init() == -1) {
         errorf("intr_init() failure");
+        return -1;
+    }
+    if (ip_init() == -1) {
+        errorf("ip_init() failure");
         return -1;
     }
     infof("initialized");
